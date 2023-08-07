c71e176ced265489a2362dd56eb976471cda5e6e
imatsatsos <ipmatsatsos@gmail.com>
2023-08-07 20:08:01 +0300

    fixed nix fontconfig

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom */
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static const int vertpad = 4;              /* vertical padding of bar */
-static const int sidepad = 2;              /* horizontal padding of bar */
+static const int sidepad = 0;              /* horizontal padding of bar */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] =
 	{
@@ -30,7 +30,7 @@ char *colors[][2] = {
 static unsigned int lines      = 0;
 /* -g option; if nonzero, dmenu uses a grid comprised of columns and lines */
 static unsigned int columns    = 0;
-static unsigned int lineheight = 31;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 static unsigned int min_lineheight = 8;
 
 /*
--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@
     nativeBuildInputs = with pkgs; [
       gnumake
       xorg.libX11.dev
-      xorg.libXft
-      xorg.libXinerama
+      xorg.libXft.dev
+      xorg.libXinerama.dev
+      fontconfig.dev
+      freetype.dev
     ];
 }
